d5447522ebd36b1322dd7a9aca4e174d29ce5d75
daixiongsheng <daixiongsheng@gmail.com>
2021-04-13 10:09:13 +0800

    2021-04-13-10:09:13 添加发布脚本

---
+++ publich.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+npm version patch
+nrm use npm
+npm publish
+nrm use taobao
--- push.sh
+++ push.sh
@@ -1,5 +1,9 @@
-#! /bin/zsh
+#! /bin/bash
+# 单测+报告生成
+npm run coverage
+# 生成目录文件
 npm run guide
+# 格式化
 npm run prettier
 git add .
 date=$(date "+%Y-%m-%d-%H:%M:%S")
