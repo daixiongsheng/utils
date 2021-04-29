8fee171422970f43458f6c3f89ccb237e594ef84
daixiongsheng <daixiongsheng@gmail.com>
2021-04-29 13:48:03 +0800

    2021-04-29-13:48:03 增加compose pipe函数

--- dist/dxs-utils.global.dev.js
+++ dist/dxs-utils.global.dev.js
@@ -1,5 +1,5 @@
 /*!
- * dxs-utils v0.0.7
+ * dxs-utils v0.0.8
  * (c) 2020-2021 Xiongsheng Dai
  * Released under the MIT License.
  */
--- publich.sh
+++ publich.sh
@@ -5,7 +5,7 @@ npm run build
 git add .
 date=$(date "+%Y-%m-%d-%H:%M:%S")
 git commit -m "$date $1"
-tag = `npm version patch`
+tag=`npm version patch`
 nrm use npm
 npm publish
 nrm use taobao
